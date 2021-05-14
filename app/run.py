8a271403aab94ec3fb428333f557bd9b0bb92a99
jeena72 <jayantsingh72@gmail.com>
2021-05-14 09:53:01 +0530

    Fix path to run app from app directory

--- app/run.py
+++ app/run.py
@@ -60,11 +60,11 @@ class StartingVerbExtractor(BaseEstimator, TransformerMixin):
 
 
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponseData', engine)
 
 # load model
-model = joblib.load("models/classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
